81621086b7577a7848aef5739d2b957eda00b673
Duani <duanielad@gmail.com>
2023-06-19 22:35:19 +0300

    Elad Changes

---
+++ src/pages/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+            <div id="nav">
+                <router-link :to="{ name: 'main' }">Vue Recipes</router-link>|
+                <router-link :to="{ name: 'search' }">Search</router-link>|
+                <span v-if="!$root.store.username">
+                Guest:
+                <router-link :to="{ name: 'register' }">Register</router-link>|
+                <router-link :to="{ name: 'login' }">Login</router-link>|
+                </span>
+                <span v-else>
+                {{ $root.store.username }}: <button @click="Logout">Logout</button>|
+                <router-link :to="{ name: 'favorites' }">Favorites</router-link>|
+                </span>
+            </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBar",
+    methods: {
+        Logout() {
+            this.$root.store.logout();
+            this.$root.toast("Logout", "User logged out successfully", "success");
+
+            this.$router.push("/").catch(() => {
+                this.$forceUpdate();
+            });
+        }
+    }
+};    
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
+// export const configureWebpack = {
+//   devtool: "source-map"
+// };
+// export const devServer = {
+//   // host: "132.72.65.211"
+//   host: "localhost"
+// };
 module.exports = {
   configureWebpack: {
     devtool: "source-map"
