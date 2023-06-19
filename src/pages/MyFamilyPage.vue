2024bac1ba9774d658f8048670307eda3913da04
turiski <turiskir@post.bgu.ac.il>
2023-06-19 19:02:35 +0300

    family&favorite-try

---
+++ src/pages/MyFamilyPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+    <h1 id="title"> My Family</h1>
+    <MyFavorite class="MyFamilyList" title="My Family" server_domain="/users/myfamilyrecipes" 
+    error_msg = "There are no favorite recipes for the connected user"/>
+    </div>
+    </template>
+    
+    <script>
+    import MyFamily from "../components/MyFamilyRecipes.vue"
+    
+    export default {
+        data(){
+            MyFamily
+        }
+    };
+    
+    </script> 
