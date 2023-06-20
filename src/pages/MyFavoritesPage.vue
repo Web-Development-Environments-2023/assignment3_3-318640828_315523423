dc7199aed285426e4dfe9b06b1f2915f042d552d
turiski <turiskir@post.bgu.ac.il>
2023-06-20 19:15:50 +0300

    Connection-Success

--- src/pages/MyFavoritesPage.vue
+++ src/pages/MyFavoritesPage.vue
@@ -1,6 +1,8 @@
 
 <template>
     <div>
+      
+
         <h2> My Favorites </h2>
       <button @click="getFavorites">Favorites</button>
       <ul>
@@ -8,24 +10,31 @@
             {{ favorites }}
         </p>
       </ul>
+
+      <RecipePreviewList :recipes="favorites" />
     </div>
   </template>
   
   <script>
+  import RecipePreviewList from "../components/RecipePreviewList.vue";
   export default {
+
     data() {
       return {
         favorites: []
       };
     },
+    components: {
+      RecipePreviewList
+    },
     methods: {
       async getFavorites() {
         try {
-            //const response = await this.axios.get(this.$root.store.server_domain + '/users/favorites');
-            const response = await this.axios.get("http://localhost:3000/users/favorites");
+            const response = await this.axios.get(this.$root.store.server_domain + "/users/favorites",
+            {withCredentials: true});
+            //const response = await this.axios.get("http://localhost:3000/users/favorites",{withCredentials: true});
             //this.$root.store.favorites = response.data;
             this.favorites = response.data;
-            this.$router.push("/");
         } catch (err) {
           console.log(err);
         }
--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,12 @@ module.exports = {
   },
   devServer: {
     // host: "132.72.65.211"
-    host: "localhost"
+    host: "127.0.0.1",
+    port: 8080
   }
+  // devServer: {
+  //   // host: "132.72.65.211"
+  //   host: "localhost"
+  // }
   // publicPath: process.env.NODE_ENV === "production" ? "/LAB12/" : "/"
 };
