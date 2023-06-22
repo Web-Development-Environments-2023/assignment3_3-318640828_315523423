5f304921a5a1cd16a0f1c3f19ac632e9dc75b9d4
DuaniE <103958872+DuaniE@users.noreply.github.com>
2023-06-22 12:01:02 +0300

    Update NavBar.vue

--- src/pages/NavBar.vue
+++ src/pages/NavBar.vue
@@ -1,22 +1,30 @@
 <template>
     <div>
-            <div id="nav">
-                <router-link :to="{ name: 'main' }">Vue Recipes</router-link>|
-                <router-link :to="{ name: 'search' }">Search</router-link>|
-                <span v-if="!$root.store.username">
-                Guest:
-                <router-link :to="{ name: 'register' }">Register</router-link>|
-                <router-link :to="{ name: 'login' }">Login</router-link>|
-                </span>
-                <span v-else>
-                {{ $root.store.username }}: <button @click="Logout">Logout</button>|
-                <router-link :to="{ name: 'favorites' }">My Favorites</router-link>|
-                <router-link :to="{ name: 'family' }">My Family</router-link>|
-                </span>
-            </div>
-        
+      <div id="nav">
+        <router-link :to="{ name: 'main' }" class="nav-link">Vue Recipes</router-link>
+        <span class="divider">|</span>
+        <router-link :to="{ name: 'search' }" class="nav-link">Search</router-link>
+        <span class="divider">|</span>
+        <span v-if="!$root.store.username" class="guest-section">
+          Guest:
+          <router-link :to="{ name: 'register' }" class="nav-link">Register</router-link>
+          <span class="divider">|</span>
+          <router-link :to="{ name: 'login' }" class="nav-link">Login</router-link>
+        </span>
+        <span v-else class="user-section">
+          <router-link :to="{ name: 'favorites' }" class="nav-link">My Favorites</router-link>
+          <span class="divider">|</span>
+          <router-link :to="{ name: 'family' }" class="nav-link">My Family</router-link>
+          <span class="divider">|</span>
+          <span class="username">{{ $root.store.username }}</span>:
+          <button class="logout-button" @click="Logout">Logout</button>
+        </span>
+      </div>
     </div>
-</template>
+  </template>
+  
+  
+  
 
 <script>
 export default {
@@ -33,3 +41,51 @@ export default {
     }
 };    
 </script>
+
+<style>
+#nav {
+  background-color: #f5f5f5;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+}
+
+#nav a {
+  margin-right: 10px;
+  color: #333;
+  text-decoration: none;
+  font-weight: bold; /* Add this line to make all links bold */
+}
+
+#nav .divider {
+  margin: 0 5px;
+  color: #333;
+}
+
+#nav .guest-section {
+  display: flex;
+  align-items: center;
+}
+
+#nav .user-section {
+  display: flex;
+  align-items: center;
+}
+
+#nav .username {
+  margin-right: 10px;
+  font-weight: bold;
+}
+
+#nav .logout-button {
+  padding: 5px 10px;
+  color: #fff;
+  background-color: #333;
+  border: none;
+  cursor: pointer;
+}
+
+.logout-button {
+  margin-left: 10px;
+}
+</style>
