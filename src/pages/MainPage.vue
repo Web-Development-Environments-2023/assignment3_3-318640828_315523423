18aa4dc953f0a241604ca962d1f420c50c66fa8e
turiski <turiskir@post.bgu.ac.il>
2023-06-23 12:52:18 +0300

    MenuAppearance

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,10 +1,28 @@
+
 <template>
-  <div class="container">
-    <h1 class="title">Main Page</h1>
-    <RecipePreviewList title="Randome Recipes" class="RandomRecipes center" />
-    <router-link v-if="!$root.store.username" to="/login" tag="button">You need to Login to vue this</router-link>
-    {{ !$root.store.username }}
-    <RecipePreviewList
+<h1 class="title">Main Page</h1>
+<div class="container">
+
+  <div class="basedOnLogin"> 
+        <h2> Last watched recipes </h2>
+        <h3> You need to Login to vue this </h3>
+        <span v-if="!$root.store.username" class="guest-section">
+          <LoginPage v-if="showLoginPage" />
+        </span> 
+        <span v-if="$root.store.username" class="user-section">
+            // the user lastviewed recipes
+
+        </span>
+  </div>  
+
+  <div class="randomRecipes">
+    <h2> Random Recipes </h2>
+    <RandomRecipePreviewList :recipes="random" />
+  </div>
+
+
+
+    <!-- <RecipePreviewList
       title="Last Viewed Recipes"
       :class="{
         RandomRecipes: true,
@@ -12,34 +30,61 @@
         center: true
       }"
       disabled
-    ></RecipePreviewList>
-    <!-- <div
-      style="position: absolute;top: 70%;left: 50%;transform: translate(-50%, -50%);"
-    >
-      Centeredasdasdad
-    </div>-->
-  </div>
+    ></RecipePreviewList> -->
+</div>
 </template>
 
+
 <script>
-import RecipePreviewList from "../components/RecipePreviewList";
+import LoginPage from './LoginPage';
+import RandomRecipePreviewList from "../components/RecipePreviewList";
 export default {
+  data() {
+    return {
+      random: [],
+      showLoginPage: true
+    };
+  },
   components: {
-    RecipePreviewList
-  }
+    RandomRecipePreviewList,
+    LoginPage
+    
+  },
+  mounted() {
+    this.getRandom();
+  },
+  methods: {
+      async getRandom() {
+        try {
+          return [];
+            const response = await this.axios.get(this.$root.store.server_domain + "/recipes/random",
+            {withCredentials: true});
+            this.random = response.data;
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
 };
 </script>
 
 <style lang="scss" scoped>
-.RandomRecipes {
+
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.basedOnLogin {
+  flex-basis: 50%;
+  margin-right: 10px;
+}
+
+.randomRecipes {
+  flex-basis: 50%;
   margin: 10px 0 10px;
+
 }
-.blur {
-  -webkit-filter: blur(5px); /* Safari 6.0 - 9.0 */
-  filter: blur(2px);
-}
-::v-deep .blur .recipe-preview {
-  pointer-events: none;
-  cursor: default;
-}
+
 </style>
